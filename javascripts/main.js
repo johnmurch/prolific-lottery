d1b78925926022ab156341409a07be7f16a36f54
John Murch <john@johnmurch.com>
2016-01-13 22:54:49 -0500

    Init Layout with Real Data Mockup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+    var generatedTickets = "";
+
+    $.each(numbers, function( index, value ) {
+      console.log(JSON.stringify(numbers[index])+"|"+numbers[index].wb1)
+      //generatedTickets+='<h1>IMAGE #"+numbers[index].tix+"</h1>';
+      generatedTickets+='<div class="clear">';
+      generatedTickets+='<div class="lott-numbers">';
+      generatedTickets+='<input type="number" value="'+numbers[index].wb1+'" disabled/>'
+      generatedTickets+='<input type="number" value="'+numbers[index].wb2+'" disabled/>'
+      generatedTickets+='<input type="number" value="'+numbers[index].wb3+'" disabled/>'
+      generatedTickets+='<input type="number" value="'+numbers[index].wb4+'" disabled/>'
+      generatedTickets+='<input type="number" value="'+numbers[index].wb5+'" disabled/>'
+      generatedTickets+='<input type="number" value="'+numbers[index].pb+'" disabled/>'
+      generatedTickets+='</div>';
+      generatedTickets+='</div>';
+    });
+    $('#tickets').html(generatedTickets);
+});
