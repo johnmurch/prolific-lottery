d9bca31098bdeee0487e2323f79e3d7c470401b3
John Murch <john@johnmurch.com>
2016-01-13 23:48:41 -0500

    Update data attr for lookup

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
     var generatedTickets = "";
 
     $.each(numbers, function( index, value ) {
-      console.log(JSON.stringify(numbers[index])+"|"+numbers[index].wb1)
-      //generatedTickets+='<h1>IMAGE #"+numbers[index].tix+"</h1>';
+      // console.log(JSON.stringify(numbers[index])+"|"+numbers[index].wb1)
       generatedTickets+='<div class="clear">';
-      generatedTickets+='<div class="lott-numbers">';
+      generatedTickets+='<div class="lott-numbers" data-ticket="'+numbers[index].tix+'" data-image="'+numbers[index].img+'">';
       generatedTickets+='<input type="number" value="'+("0" + numbers[index].wb1).slice (-2)+'" disabled/>'
       generatedTickets+='<input type="number" value="'+("0" + numbers[index].wb2).slice (-2)+'" disabled/>'
       generatedTickets+='<input type="number" value="'+("0" + numbers[index].wb3).slice (-2)+'" disabled/>'
